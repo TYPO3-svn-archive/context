9211c85f39bf209ba82c70533de38f6a21b7c68c
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-20 09:36:06 +0000

    Initial import of Kickstarter code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/context/trunk@25615 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "context".
+#
+# Auto generated 20-10-2009 11:33
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Context Manager',
+	'description' => 'Provides a general framework to define contexts for parts of the page tree. Other extensions may use these contexts as needed.',
+	'category' => 'fe',
+	'author' => 'Francois Suter (Cobweb)',
+	'author_email' => 'typo3@cobweb.ch',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"24f6";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"2d51";s:16:"locallang_db.xml";s:4:"0445";s:19:"doc/wizard_form.dat";s:4:"0fdc";s:20:"doc/wizard_form.html";s:4:"537d";s:38:"modfunc1/class.tx_context_modfunc1.php";s:4:"7a70";s:22:"modfunc1/locallang.xml";s:4:"c3a1";}',
+);
+
+?>
