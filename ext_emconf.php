df3bb374c91584d3e4428babe6a67867da94dc4e
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-10 08:17:15 +0000

    Released to TER as version 1.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/context/trunk@64331 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "context".
 #
-# Auto generated 30-01-2012 10:46
+# Auto generated 10-07-2012 10:16
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-4.7.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"7d7e";s:10:"README.txt";s:4:"c12b";s:20:"class.tx_context.php";s:4:"ffab";s:16:"ext_autoload.php";s:4:"2f54";s:12:"ext_icon.gif";s:4:"b6d8";s:17:"ext_localconf.php";s:4:"0bf1";s:16:"locallang_db.xml";s:4:"0445";s:14:"doc/manual.pdf";s:4:"65ac";s:14:"doc/manual.sxw";s:4:"bd4d";s:14:"doc/manual.txt";s:4:"01de";s:50:"interfaces/interface.tx_context_contextstorage.php";s:4:"0c4a";s:25:"tests/tx_context_Test.php";s:4:"1810";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"4ae3";s:20:"class.tx_context.php";s:4:"ffab";s:16:"ext_autoload.php";s:4:"2f54";s:12:"ext_icon.gif";s:4:"b6d8";s:17:"ext_localconf.php";s:4:"0bf1";s:16:"locallang_db.xml";s:4:"0445";s:10:"README.txt";s:4:"c12b";s:14:"doc/manual.pdf";s:4:"815d";s:14:"doc/manual.sxw";s:4:"cb36";s:14:"doc/manual.txt";s:4:"ec0e";s:50:"interfaces/interface.tx_context_contextstorage.php";s:4:"0c4a";s:25:"tests/tx_context_Test.php";s:4:"1810";}',
 	'suggests' => array(
 	),
 );
